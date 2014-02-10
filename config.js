a410a20abb0db9f12f651989a6ff9047771358f0
Topher Marie <topher@jumpcloud.com>
2014-02-10 11:09:51 -0700

    Factoring out into a settings file

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {}
+
+config.target = {};
+
+config.target.host = 'alert.victorops.com';
+config.target.port = 443;
+config.target.path = '/integrations/generic/20131114/alert/33aba56f-fc78-4c65-9c6d-57a49cbb6ed8/uservoicetest';
+config.target.method = 'POST';
+
+module.exports = config;
